44662b09c03b8a3e8ea1267f1d8cf435fdac7333
AshishDara <97780553+AshishDara@users.noreply.github.com>
2025-09-02 23:39:28 +0530

    style: Refactor import statements and improve comments in documentRoutes.js

--- backend/routes/documentRoutes.js
+++ backend/routes/documentRoutes.js
@@ -1,13 +1,18 @@
 // routes/documentRoutes.js
 import express from 'express';
 import { protect } from '../middleware/authMiddleware.js';
-import { createDocument, getUserDocuments,addCollaborator, } from '../controllers/documentController.js';
+import {
+    createDocument,
+    getUserDocuments,
+    addCollaborator,
+} from '../controllers/documentController.js';
 
 const router = express.Router();
 
-// Apply the 'protect' middleware to both routes
-router.route('/')
-  .post(protect, createDocument)
-  .get(protect, getUserDocuments);
+//routes for the base path '/' (e.g., GET /api/docs and POST /api/docs)
+router.route('/').post(protect, createDocument).get(protect, getUserDocuments);
+
+//the route for adding collaborators to a specific document
 router.route('/:documentId/collaborators').post(protect, addCollaborator);
+
 export default router;
